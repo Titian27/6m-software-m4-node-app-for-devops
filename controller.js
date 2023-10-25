d6ea5db21601f86f99f6a1732c045bdf261c53af
titian27 <huixuanxz@gmail.com>
2023-10-25 15:23:47 +0800

    docker update

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 
 // This is the only function that will be called by the endpoint.
 function print(req, res){
-    res.send("Hello world, huixuan!!");
+    res.send("Hello world,huixuan!!");
 }
 
 module.exports = print;
